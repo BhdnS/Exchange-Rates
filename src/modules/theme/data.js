170804f76cc63e3651d549891a7b56a73aa954ff
Bohdan <121521972+BhdnS@users.noreply.github.com>
2023-06-30 00:39:43 +0300

    Update data.js

--- src/modules/theme/data.js
+++ src/modules/theme/data.js
@@ -4,7 +4,7 @@ const theme = new Theme({
   img: document.querySelector('.theme'),
   title: document.querySelector('.header__title'),
   btnDownload: document.querySelector('.btn__download'),
-	btnView: document.querySelector('.btn__view'),
+  btnView: document.querySelector('.btn__view'),
   table: document.querySelector('.main__table'),
   body: document.querySelector('body'),
 });
